52d4f2773697754ff23344ed5fc69eebcf74db77
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-04-30 10:19:01 +0200

    'core'

---
+++ AssessmentDeliveryTestFramework/Core/Driver/PlatformUtils/IPlatformDriverUtils.cs
@@ -0,0 +1,18 @@
+﻿using AssessmentDeliveryTestingFramework.Core.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessmentDeliveryTestingFramework.Core.Driver.PlatformUtils
+{
+    public interface IPlatformDriverUtils
+    {
+        void TearDownDriver(List<IDriverContainer> sessionDrivers, string browserType);
+
+        List<int> GetDriversProcessesId(string browserType);
+
+        void TerminateProcess(string processName);
+    }
+}
