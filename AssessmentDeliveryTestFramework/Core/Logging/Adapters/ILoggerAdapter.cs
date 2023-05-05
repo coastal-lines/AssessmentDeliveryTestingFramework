b5007b99c9cfa5a77eb1c80d07a2daf2adb98029
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-05-05 10:14:43 +0200

    'log'

---
+++ AssessmentDeliveryTestFramework/Core/Logging/Adapters/ILoggerAdapter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessmentDeliveryTestFramework.Core.Logging.Adapters
+{
+    public interface ILoggerAdapter
+    {
+        void LogInformation(string message);
+
+        void LogError(string message, Exception ex);
+        // Add other log methods as needed
+    }
+}
