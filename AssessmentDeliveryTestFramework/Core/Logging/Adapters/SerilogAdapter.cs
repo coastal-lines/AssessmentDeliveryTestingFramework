875778c7cd7e287ff72d14b1534582e73f6af910
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-05-06 11:45:10 +0200

    'log'

---
+++ AssessmentDeliveryTestFramework/Core/Logging/Adapters/SerilogAdapter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessmentDeliveryTestFramework.Core.Logging.Adapters
+{
+    using Serilog;
+
+    public class SerilogAdapter : ILoggerAdapter
+    {
+        private readonly ILogger logger;
+
+        public SerilogAdapter()
+        {
+            // Initialize your Serilog logger here
+            this.logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+        }
+
+        public void LogInformation(string message)
+        {
+            this.logger.Information(message);
+        }
+
+        public void LogError(string message, Exception ex)
+        {
+            this.logger.Error(ex, message);
+        }
+        // Implement other log methods
+    }
+}
