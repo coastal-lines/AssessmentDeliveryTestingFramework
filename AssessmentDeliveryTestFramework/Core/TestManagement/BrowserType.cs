b47ca65a3e0277dceeaa281e5002f516bc8aa5de
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-05-11 19:10:14 +0200

    'core'

---
+++ AssessmentDeliveryTestFramework/Core/TestManagement/BrowserType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AssessmentDeliveryTestingFramework.Core.TestManagement
+{
+    public static class BrowserType
+    {
+        public const string Firefox = "Firefox";
+
+        public const string Chrome = "Chrome";
+
+        public const string Remote = "Remote";
+
+        public const string Min = "Min";
+    }
+}
---
+++ AssessmentDeliveryTestFramework/Core/TestManagement/PlatformTypes.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessmentDeliveryTestingFramework.Core.TestManagement
+{
+    public static class PlatformTypes
+    {
+        public const string Windows = nameof(PlatformType.Windows);
+
+        public const string Android = nameof(PlatformType.Android);
+    }
+}
