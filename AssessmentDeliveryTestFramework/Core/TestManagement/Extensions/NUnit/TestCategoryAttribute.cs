1d2f8df78dd1315712b599df1d02b0bd49587212
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-05-12 18:58:37 +0200

    'core'

---
+++ AssessmentDeliveryTestFramework/Core/TestManagement/Extensions/NUnit/TestCategoryAttribute.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+using NUnit.Framework;
+
+namespace AssessmentDeliveryTestingFramework.Core.TestManagement.Extensions.NUnit
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Assembly, AllowMultiple = true, Inherited = true)]
+    public class BrowserTypeAttribute : NUnitAttribute, IApplyToTest
+    {
+        public BrowserTypeAttribute(string name) => Name = name.Trim();
+
+        public string Name { get; }
+
+        public void ApplyToTest(Test test)
+        {
+            test.Properties.Add("Category", Name);
+            test.Properties.Add("BrowserType", Name);
+        }
+    }
+}
