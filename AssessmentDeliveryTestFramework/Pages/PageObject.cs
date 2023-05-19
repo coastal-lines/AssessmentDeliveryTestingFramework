341da1cf98ebb2bba3a133a6bc7e3e54b5306fe1
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-05-19 05:11:02 +0200

    'core'

---
+++ AssessmentDeliveryTestFramework/Pages/PageObject.cs
@@ -0,0 +1,29 @@
+﻿using AssessmentDeliveryTestingFramework.Core;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace AssessmentDeliveryTestingFramework.Page
+{
+    /*
+    [TestFixture]
+    [FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+    public class WebPageObject
+    {
+        private WebSession _session;
+
+        public WebSession Session => _session;
+
+        [SetUp]
+        public void Setup()
+        {
+            _session = new WebSession();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _session.GetDriver().Quit();
+        }
+    }
+    */
+}
