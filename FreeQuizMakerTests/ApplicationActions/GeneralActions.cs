7ff3980fca5608776943b5c07a87b662146824e2
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-05-23 10:44:39 +0200

    'core'

---
+++ FreeQuizMakerTests/ApplicationActions/GeneralActions.cs
@@ -0,0 +1,34 @@
+﻿using FreeQuizMakerTests.Pages.FreeQuizMakerExams.ControlsPane;
+using FreeQuizMakerTests.Pages.FreeQuizMakerExams.QuestionDetailsPane;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeQuizMakerTests.ApplicationActions
+{
+    public class GeneralActions
+    {
+        private WindowsDriver _driver;
+
+        private ToolsPage _toolsPage;
+
+        public ToolsPage ToolsPage => _toolsPage ?? new ToolsPage(_driver);
+
+        public GeneralActions(WindowsDriver driver)
+        {
+            _driver = driver;
+            _toolsPage = new ToolsPage(driver);
+        }
+
+        public QuestionDetailsPage CreateNewQuestion()
+        {
+            ToolsPage.ClickNewQuestionButton();
+            Thread.Sleep(4000);
+            return new QuestionDetailsPage(_driver);
+        }
+    }
+}
