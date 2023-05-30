492e2374b44be70b4c8e51750af8556e8950215c
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-05-30 16:11:30 +0200

    'desktop'

---
+++ FreeQuizMakerTests/Pages/FreeQuizMakerExams/QuestionDetailsPane/QuestionDetailsPage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeQuizMakerTests.Pages.FreeQuizMakerExams.QuestionDetailsPane
+{
+    public class QuestionDetailsPage : DesktopWindowsBasePage
+    {
+        private IWebElement QuestionEdit => WebElementWaiting.WaitElement(By.XPath("//Edit[@AutomationId='txtQuestion']"));
+
+        public QuestionDetailsPage(WindowsDriver driver) : base(driver)
+        {
+
+        }
+
+        public string GetQuestionEditText()
+        {
+            return QuestionEdit.Text;
+        }
+    }
+}
