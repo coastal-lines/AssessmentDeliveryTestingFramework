f1e987039a44191bf044ceaf4aed1407c06a0bc6
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-06-14 19:53:39 +0200

    'web'

---
+++ GorillaTests/RemoteTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GorillaTests
+{
+    internal class RemoteTest
+    {
+        /*
+        [Test]
+        public void RemoteTestFirefox()
+        {
+            var options = new FirefoxOptions();
+            var driver = new RemoteWebDriver(new Uri("http://192.168.43.78:9247"), options);
+            driver.Navigate().GoToUrl("https://stackoverflow.com/questions/10607806/bind-selenium-to-a-specific-ip-possible");
+        }
+
+        [Test]
+        public void TestFirefox()
+        {
+            var options = new FirefoxOptions();
+            var driver = new RemoteWebDriver(new Uri("http://192.168.43.78:9247"), options);
+            driver.Navigate().GoToUrl("https://stackoverflow.com/questions/10607806/bind-selenium-to-a-specific-ip-possible");
+        }
+
+        [Test]
+        public void RemoteTestChrome()
+        {
+            var options = new ChromeOptions();
+            var driver = new RemoteWebDriver(new Uri("http://192.168.43.78:9247"), options);
+            driver.Navigate().GoToUrl("https://stackoverflow.com/questions/10607806/bind-selenium-to-a-specific-ip-possible");
+        }
+        */
+    }
+}
---
+++ GorillaTests/SimpleGorillaTests.cs
@@ -0,0 +1,85 @@
+﻿using AssessmentDeliveryTestingFramework.Core.TestManagement;
+using NUnit.Framework;
+using AssessmentDeliveryTestingFramework.Page;
+using GorillaTests.Pages.GorillaExams;
+using AssessmentDeliveryTestingFramework.Core.TestManagement.Extensions.NUnit;
+
+namespace GorillaTests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class SimpleGorillaTests : WebPageObject
+    {
+        [Test(Description = "SimpleTest")]
+
+        [BrowserType("Firefox")]
+        [Category("Web")]
+        public void SimpleTest()
+        {
+            Session.GetDriver().Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            Session.GetDriver().Navigate().GoToUrl("https://www.example.com");
+            Console.WriteLine("Test executed on thread " + Thread.CurrentThread.ManagedThreadId);
+            Thread.CurrentThread.Join(3000);
+        }
+
+        /*
+        [Test(Description = "TC2")]
+        [Category(BrowserType.Chrome)]
+        public void Test2()
+        {
+            //var examBase = new BaseFixtures(Session.GetDriver());
+            //var examBaseComponent = examBase.NavigateIntoExam();
+            //examBaseComponent.SelectMcqAnswer();
+            //Thread.CurrentThread.Join(1000);
+        }
+        */
+
+        /*
+        [Test(Description = "TC3")]
+        [Category(BrowserType.Firefox)]
+        public void Test3()
+        {
+            var examBase = new BaseFixtures(Session.GetDriver(), Session.GetWait());
+            var examBaseComponent = examBase.NavigateIntoExam();
+            examBaseComponent.SelectMcqAnswer();
+            Thread.CurrentThread.Join(15000);
+        }
+        */
+
+        /*
+        [Test(Description = "TC4")]
+        [Category(BrowserType.Min)]
+        public void Test4()
+        {
+            Session.GetDriver().Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            Thread.CurrentThread.Join(5000);
+            Console.WriteLine("Test executed on thread " + Thread.CurrentThread.ManagedThreadId);
+            Session.GetDriverFeatures<ElectronBrowserFeatures>().NavigateToUrl("https://www.testgorilla.com/");
+
+            Assert.AreEqual("Talent Assessments for Skills-Based Hiring - TestGorilla", Session.GetDriver().Title, $"The {"https://www.testgorilla.com/"} page has a wrong title.");
+        }
+        */
+
+        /*
+        [Test(Description = "TC5")]
+        [Category(BrowserType.Chrome)]
+        [Category(PlatformTypes.Android)]
+        public void Test4()
+        {
+            Session.GetDriver().Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            Thread.CurrentThread.Join(5000);
+            Console.WriteLine("Test executed on thread " + Thread.CurrentThread.ManagedThreadId);
+            Session.GetDriverFeatures<ElectronBrowserFeatures>().NavigateToUrl("https://www.testgorilla.com/");
+
+            Assert.AreEqual("Talent Assessments for Skills-Based Hiring - TestGorilla", Session.GetDriver().Title, $"The {"https://www.testgorilla.com/"} page has a wrong title.");
+        }
+        */
+    }
+}
+
+/*
+            Session.GetDriver().Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            Session.GetDriver().Navigate().GoToUrl("https://www.example.com");
+            Thread.CurrentThread.Join(5000);
+            Console.WriteLine("Test executed on thread " + Thread.CurrentThread.ManagedThreadId);
+*/
