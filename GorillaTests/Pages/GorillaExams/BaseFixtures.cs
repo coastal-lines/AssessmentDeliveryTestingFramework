d5876f0e0f91ee3324ce2b0e90cab6824f9ee25d
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-06-18 18:41:00 +0200

    'web'

---
+++ GorillaTests/Pages/GorillaExams/BaseFixtures.cs
@@ -0,0 +1,22 @@
+﻿using AssessmentDeliveryTestingFramework.Core;
+using AssessmentDeliveryTestingFramework.Core.Wait;
+using OpenQA.Selenium;
+
+namespace GorillaTests.Pages.GorillaExams
+{
+    public class BaseFixtures : WebBasePage
+    {
+        public BaseFixtures(IWebDriver driver) : base(driver) 
+        { 
+        
+        }
+
+
+        public ExamBaseComponent NavigateIntoExam()
+        {
+            Driver.Navigate().GoToUrl("https://app.testgorilla.com/preview/7aee275a-8df7-469f-98b2-68ea44c994e4?language=en");
+
+            return new ExamBaseComponent(Driver);
+        }
+    }
+}
---
+++ GorillaTests/Pages/GorillaExams/BasePage.cs
@@ -0,0 +1,28 @@
+﻿using AssessmentDeliveryTestingFramework.Core.Wait;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GorillaTests.Pages.GorillaExams
+{
+    public class WebBasePage
+    {
+        private IWebDriver _driver;
+
+        private WebElementWaiting _elementWait;
+
+        public IWebDriver Driver => _driver;
+
+        public WebElementWaiting WebElementWaiting => _elementWait;
+
+        public WebBasePage(IWebDriver driver)
+        {
+            _driver = driver ?? throw new ArgumentNullException(nameof(driver)); //TODO message
+
+            _elementWait = new WebElementWaiting(driver);
+        }
+    }
+}
