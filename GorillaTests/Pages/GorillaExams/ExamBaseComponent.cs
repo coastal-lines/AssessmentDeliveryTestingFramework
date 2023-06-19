5273b5b8313da81d79b820ce8a39527a6079f4f3
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-06-19 15:41:52 +0200

    'web'

---
+++ GorillaTests/Pages/GorillaExams/ExamBaseComponent.cs
@@ -0,0 +1,29 @@
+﻿using AssessmentDeliveryTestingFramework.Core;
+using AssessmentDeliveryTestingFramework.Core.Wait;
+using OpenQA.Selenium;
+
+namespace GorillaTests.Pages.GorillaExams
+{
+    public class ExamBaseComponent : WebBasePage
+    {
+        private ExamNavigationComponent _examNavigationComponent;
+
+        private ExamToolsComponent _examToolsComponent;
+
+        private IList<IWebElement> _ListAnswersMCQ => WebElementWaiting.WaitElements(By.TagName("app-tgo-choice"));
+
+        public ExamBaseComponent(IWebDriver driver) : base(driver) 
+        { 
+        
+        }
+
+        public ExamNavigationComponent ExamNavigationComponent => _examNavigationComponent ?? new ExamNavigationComponent(Driver);
+
+        public ExamToolsComponent ExamToolsComponent => _examToolsComponent ??= new ExamToolsComponent();
+
+        public void SelectMcqAnswer(int answerIndex = 0)
+        {
+            _ListAnswersMCQ[answerIndex].Click();
+        }
+    }
+}
---
+++ GorillaTests/Pages/GorillaExams/ExamNavigationComponent.cs
@@ -0,0 +1,21 @@
+﻿using AssessmentDeliveryTestingFramework;
+using AssessmentDeliveryTestingFramework.Core.Wait;
+using OpenQA.Selenium;
+
+namespace GorillaTests.Pages.GorillaExams
+{
+    public class ExamNavigationComponent : WebBasePage
+    {
+        private IWebElement _ImageLogo => WebElementWaiting.WaitElement(By.XPath("//content//img[@src='assets/tgo-logo.jpeg']"));
+
+        public ExamNavigationComponent(IWebDriver driver) : base(driver)
+        {
+            
+        }
+
+        public bool IsLogoImageDisplayed()
+        {
+            return _ImageLogo.Displayed;
+        }
+    }
+}
