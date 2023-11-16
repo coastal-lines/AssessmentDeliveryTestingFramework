26a3e4351b7f5afc684b01ebc5d4f8ddf23981a4
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-11-16 11:55:35 +0100

    ''

---
+++ Mobile/Android/AptitudeTests/Pages/AptitudeExams/TestScreen.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Appium.Interfaces;
+using OpenQA.Selenium.Appium;
+
+namespace AptitudeTests.Pages.AptitudeExams
+{
+    public class TopMenuScreen
+    {
+        private AndroidDriver _androidDriver;
+
+        public TopMenuScreen(AndroidDriver androidDriver)
+        {
+            _androidDriver = androidDriver;
+        }
+    }
+
+    public class BottomMenuScreen
+    {
+        private AndroidDriver _androidDriver;
+
+        public BottomMenuScreen(AndroidDriver androidDriver)
+        {
+            _androidDriver = androidDriver;
+        }
+    }
+
+    public class TestMainViewScreen
+    {
+        private AndroidDriver _androidDriver;
+
+        private IWebElement MainWebViewElement => _androidDriver.FindElement(By.XPath("//android.webkit.WebView"));
+
+        private IWebElement QuestionText => MainWebViewElement.FindElement(By.XPath($"//android.view.View[@text='']"));
+
+        public TestMainViewScreen(AndroidDriver androidDriver)
+        {
+            _androidDriver = androidDriver;
+        }
+    }
+
+    public class TestScreen
+    {
+        private AndroidDriver _androidDriver;
+
+        public TestScreen(AndroidDriver androidDriver)
+        {
+            _androidDriver = androidDriver;
+        }
+    }
+}
---
+++ Mobile/Android/AptitudeTests/Utils/AdsPopupObserver.cs
@@ -0,0 +1,61 @@
+﻿using AssessmentDeliveryTestingFramework.Core.Session;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Appium.Android;
+
+namespace AptitudeTests.Utils
+{
+    public class AdsPopupObserver
+    {
+        private AndroidDriver _driver;
+
+        private static Thread _additionalScreenThread;
+
+        private bool _isPopup = true;
+
+        public AdsPopupObserver(AndroidDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void StartAdsMonitor()
+        {
+            _additionalScreenThread = new Thread(MonitorAdditionalScreens);
+
+            _additionalScreenThread.Start();
+
+            _additionalScreenThread.Join();
+        }
+
+        public void MonitorAdditionalScreens()
+        {
+            while (_isPopup)
+            {
+                try
+                {
+                    if (_driver.FindElements(By.XPath("//android.widget.Button[@text='Update']")).FirstOrDefault().Displayed)
+                    {
+                        byte[] imageArray = File.ReadAllBytes(@"Resources\CloseAdsButton.jpg");
+                        string base64ImageRepresentation = Convert.ToBase64String(imageArray);
+
+                        var imgEl2 = _driver.FindElement(MobileBy.Image(base64ImageRepresentation));
+                        imgEl2.Click();
+
+                        _isPopup = false;
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
